daee5aa4bd0d3bac1fec6183a69b99df7ae15208
Venkatesan Panneerselvam <venkat.selvam@agoda.com>
2023-05-10 15:59:13 +0700

    creating docker image by docker file

---
+++ 02_creating_container/myapp/index.js
@@ -0,0 +1,12 @@
+var exp = require('express');
+var app = exp()
+
+app.get('/', function(req, res){
+res.send('hello wrod!');
+});
+
+var server = app.listen(3000, function(){
+	var h = server.address().address;
+	var p = server.address().port;
+	console.log('example app listening at http://%s%s', h, p);
+});
